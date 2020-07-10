de76b86f47079ce2946966f3cc4b24446678b01f
Luis Alejandro Lucena <luis.lucena89@gmail.com>
2020-07-10 14:27:34 +0000

    se incluyen breackpoints en estilos para los contenedores

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -4,13 +4,13 @@ import React from 'react';
 
 export const Navbar = () => {
     return (
-        <div className="nav-container">
+        <nav className="nav-container">
             <div className="coldy-logo">coldy App</div>
             <div className="main-search">
-                <button type="button" className="btn-primary">Buscar</button>
+                <button type="button" className="btn-info">Buscar</button>
                 <div>seach bar</div>
             </div>
             <div className="user-box">user</div>
-        </div>
+        </nav>
     );
 }
