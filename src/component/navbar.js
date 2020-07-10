ad2b6f31902d28be5ad5834b21446ac8b21553cc
Luis Alejandro Lucena <luis.lucena89@gmail.com>
2020-07-10 00:17:02 -0400

    se parcializan estilos scss, se comienza a trabajar en estilos del navbar superior

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -7,7 +7,7 @@ export const Navbar = () => {
         <div className="nav-container">
             <div className="coldy-logo">coldy App</div>
             <div className="main-search">
-                <button type="button" className="btn btn-primary">Buscar</button>
+                <button type="button" className="btn-primary">Buscar</button>
                 <div>seach bar</div>
             </div>
             <div className="user-box">user</div>
