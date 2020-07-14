76338af3c07f11254d31d52bab2ab119df647d4b
Luis Alejandro Lucena <luis.lucena89@gmail.com>
2020-07-14 00:10:24 -0400

    se modifica set_role function

--- src/store/flux.js
+++ src/store/flux.js
@@ -4,6 +4,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 		store: {
             global_role: "admin",
             user: {
+                id: 0,
                 fname:"Luis",
                 lname:"Lucena",
                 company: "Lider Frio",
@@ -29,23 +30,20 @@ const getState = ({ getStore, getActions, setStore }) => {
             },
             set_role: (role) => {
                 const store = getStore();
-                let pass = false;
-                if (typeof(role) === undefined) {
-                    setStore({global_role: store.roles[0].id})
-                } else {
-                    const new_roles = store.roles.map(item => {
-                        if (item.id === role) {
-                            pass = true;
-                            return {...item, active: true}
-                        } else {
-                            return {...item, actve: false}
-                        }
-                    });
-                    if (pass) {
-                        setStore({global_role: role, roles: new_roles});
+                role = typeof(role) !== 'undefined' ? role : store.roles[0].id;
+
+                const new_roles = store.roles.map(item => {
+                    if (item.id === role) {
+                        pass = true;
+                        return {...item, active: true}
                     } else {
-                        console.log("bad role request!...");
+                        return {...item, actve: false}
                     }
+                });
+                if (pass) {
+                    setStore({global_role: role, roles: new_roles});
+                } else {
+                    console.log("bad role request!...");
                 }
             }
 		}
