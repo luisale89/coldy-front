4930bff4525070406a5334ba757e3b178c54e757
Luis Alejandro Lucena <luis.lucena89@gmail.com>
2020-07-16 00:14:53 -0400

    se configuran variables y entorno para cambiar las vistas según la navegación del usuario

--- src/component/sidebar.js
+++ src/component/sidebar.js
@@ -6,22 +6,21 @@ export const Sidebar = () => {
     // eslint-disable-next-line
     const {store, actions} = useContext(Context);
 
-    const admin_links = [
-        {name:"Inicio", to: "/", logo: 'fas fa-home'},
-        {name:"Usuarios", to: "/usuarios", logo: 'fas fa-users'},
-        {name:"Clientes", to: "/clientes", logo: 'fas fa-house-user'},
-        {name:"Reportes", to: "/reportes", logo: 'far fa-chart-bar'},
-        {name:"Alarmas", to:"/alarmas", logo: 'fas fa-exclamation-triangle'},
-        {name:"Planificación", to:"/planificacion", logo:'far fa-calendar-alt'}
-    ];
-    //eslint-disable-next-line
-    const mante_links = [
-        {name:"Inicio", to:"/mantenedor", logo:""},
-        {name:"Mantenimiento", to:"/mantenedor/mtto", logo:""},
-        {name:"Mis Reportes", to:"/mantenedor/reportes", logo:""},
-        {name:"Alarmas", to:"/mantenedor/alarmas", logo:""},
-        {name:"Tareas", to:"/mantenedor/tareas", logo:""},
-    ];
+    const app_links = [ //en esta variable se definen todas las rutas de la app. role va a mostrar las correspondientes.
+        //admin links
+        {role: 0, name: "Inicio", to: `/${store.app_roles[0].name}`, logo: 'fas fa-home'},
+        {role: 0, name: "Usuarios", to: `/${store.app_roles[0].name}/usuarios`, logo: 'fas fa-users'},
+        {role: 0, name: "Clientes", to: `/${store.app_roles[0].name}/clientes`, logo: 'fas fa-house-user'},
+        {role: 0, name: "Reportes", to: `/${store.app_roles[0].name}/reportes`, logo: 'far fa-chart-bar'},
+        {role: 0, name: "Alarmas", to: `/${store.app_roles[0].name}/alarmas`, logo: 'fas fa-exclamation-triangle'},
+        {role: 0, name: "Planificación", to: `/${store.app_roles[0].name}/planificacion`, logo: 'far fa-calendar-alt'},
+        //tech links
+        {role: 1, name: "Inicio", to: `/${store.app_roles[1].name}`, logo: 'fas fa-home'},
+        {role: 1, name: "Mantenimiento", to: `/${store.app_roles[1].name}/mantenimiento`, logo: 'fas fa-home'},
+        {role: 1, name: "Mis Reportes", to: `/${store.app_roles[1].name}/reportes`, logo: 'fas fa-home'},
+        {role: 1, name: "Alarmas", to: `/${store.app_roles[1].name}/alarmas`, logo: 'fas fa-home'},
+        {role: 1, name: "Tareas", to: `/${store.app_roles[1].name}/tareas`, logo: 'fas fa-home'},
+    ]
 
     return (
         <div id="sideNav" className={store.side_bar ? "active":""}>
@@ -29,20 +28,38 @@ export const Sidebar = () => {
             <div id="role-select" className="btn-group">
                 {store.user.roles.map(item => {
                     return (
-                        <NavLink to={`/${item.id}`} key={item.id} className="select-item"activeClassName="active">{item.name}</NavLink>
-                    )
-                })}
-            </div>
-            <div className="nav-zone">
-                {admin_links.map(item => {
-                    return (
-                        <NavLink key={item.name} to={item.to} onClick={actions.close_sidebar} activeClassName="active-nav" exact>
-                            <i className={item.logo}></i>
-                            <span>{item.name}</span>
+                        <NavLink 
+                            to={`/${store.app_roles[item.role].name}`} 
+                            key={item.role} 
+                            className="select-item"
+                            activeClassName="active"
+                            onClick={() => actions.set_role(item.role)}
+                            >
+                                {item.name}
                         </NavLink>
                     )
                 })}
             </div>
+            <div className="nav-zone">
+                {app_links.map(item => {
+                    if (item.role === store.current_role) {
+                        return (
+                            <NavLink 
+                                key={item.to} 
+                                to={item.to} 
+                                onClick={actions.close_sidebar} 
+                                activeClassName="active-nav" 
+                                exact
+                                >
+                                    <i className={item.logo}></i>
+                                    <span>{item.name}</span>
+                            </NavLink>
+                        )
+                    } else {
+                        return null;
+                    }
+                })}
+            </div>
         </div>
-    )
+    );
 }
--- src/store/flux.js
+++ src/store/flux.js
@@ -1,18 +1,24 @@
+//eslint-disable-next-line
 import history from "../views/history";
 
 //eslint-disable-next-line
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-            global_role: "",
+            app_roles: [
+                {id: 0, name: "administrador"},
+                {id: 1, name: "mantenedor"},
+                {id: 2, name: "cliente"}
+            ],
+            current_role: 0,
             user: {
                 id: 0,
                 fname:"Luis",
                 lname:"Lucena",
                 company: "Lider Frio",
                 roles:[
-                    {id:"administrador", name:"Administrador", active: true},
-                    {id:"mantenedor", name:"Técnico", active: false}
+                    {role: 0, name:"Administrador"},
+                    {role: 1, name:"Técnico"}
                 ]
             },
             side_bar: false // tag para mostrar/ocultar sidebar desde el navbar.
@@ -30,24 +36,15 @@ const getState = ({ getStore, getActions, setStore }) => {
             open_sidebar: () => {
                 setStore({side_bar: true});
             },
-            set_role: (role) => {
+            set_role: (role) => { // establece el rol global de la app para las configuraciones.
                 const store = getStore();
-                role = typeof(role) !== 'undefined' ? role : store.user.roles[0].id;
-                let pass = false;
+                role = typeof(role) !== 'undefined' ? role : store.user.roles[0].role;
 
-                const new_roles = store.user.roles.map(item => {
-                    if (item.id === role) {
-                        pass = true;
-                        return {...item, active: true}
-                    } else {
-                        return {...item, active: false}
+                store.user.roles.forEach(item => {
+                    if (item.role === role) {
+                        setStore({current_role: role});
                     }
                 });
-                if (pass) {
-                    setStore({global_role: role, roles: new_roles});
-                } else {
-                    console.log("bad role request!...");
-                }
             }
 		}
 	};
