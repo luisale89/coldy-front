6b85d2bb90e01f587117a2396e92257856f82953
Luis Alejandro Lucena <luis.lucena89@gmail.com>
2020-07-20 02:43:46 +0000

    se completa la validación del formulario de ingreso con las nuevas funciones de validación. Sigiente paso será el formulario de registro

--- src/helper/validations.js
+++ src/helper/validations.js
@@ -1,26 +1,36 @@
+export const fb_styles= { // util para mostrar feedback al usuario en los formularios.
+    valid: "invalid-tooltip",
+    invalid: "invalid-tooltip active"
+};
+
+const valid_types = ["text", "email", "password"];
+
 const validations  = {
     email: (email) => { // cuando el tipo del campo a ser validado es email.
         const reEmail = /\S+@\S+\.\S+/; //expresion regular para verificar email.
         if (reEmail.test(email)) {
-            return {valid: true, msg: "ok", class: "valid"}
+            return {valid: true, feedback: {class: fb_styles.valid, msg: "Ok"}}
         } else {
-            return {valid: false, msg: "Email inválido", class: "invalid"}
+            return {valid: false, feedback: {class: fb_styles.invalid, msg: "email inválido"}}
         }
     },
     password: (passw) => { // cuando el tipo del campo a ser validado es password.
         const rePassw = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/; //expresion regular para verificar contraseña
         passw = typeof(passw) !== 'undefined' ? passw : "invalid";
         if (rePassw.test(passw)) {
-            return {valid: true, msg: "ok", class: "valid"}
+            return {valid: true, feedback: {class: fb_styles.valid, msg: "Ok"}}
         } else {
-            return {valid: false, msg: "Contraseña inválida", class: "invalid"}
+            return {valid: false, feedback: {class: fb_styles.invalid, msg: "Contraseña inválida"}}
         }
     },
     text: (text) => { // cuando el tipo del campo a ser validado es text.
-        if (text !== "" && text !== undefined) {
-            return {valid: true, msg: "ok", class: "valid"}
+        const reText = /[^a-zA-Z -]/; //sin caracteres especiales
+        if (text.trim() === "") {
+            return {valid: false, feedback: {msg: "Completa este campo", class: fb_styles.invalid}}
+        } else if (reText.test(text)){
+            return {valid: false, feedback: {msg: "Inválido", class: fb_styles.invalid}}
         } else {
-            return {valid: false, msg: "Campo requerido", class: "invalid"}
+            return {valid: false, feedback: {msg: "ok", class: fb_styles.valid}}
         }
     }
 };
@@ -29,17 +39,10 @@ export const validate_field = (type, value) => {
     type = typeof(type) !== 'undefined' ? type : "text"; //default type for validations
     value = typeof(value) !== 'undefined' ? value : ""; //default value for validate
 
-    if (type === "email" || type === "password" || type === "text") {
+    if (valid_types.includes(type)) {
         return validations[type](value);
-    } else {
-        return {valid: false, msg: "Invalid Field", class:"invalid"}
     }
-    // funcion que se encarga de validar a través de type, id (que coincida con los requeridos)
-    // y un valor. si id = * se revisa la validación de todos los campos requeridos.
-
 };
-
-const valid_types = ["text", "email", "password"];
 
 export const validate_repass = (pass, repass) => {
     pass = typeof(pass) !== 'undefined' ? pass : "valid";
@@ -54,16 +57,17 @@ export const validate_repass = (pass, repass) => {
 
 export const validate_all = (form_id) => { // will return an object with an array of valids and a object of feedback.
     const ele = document.getElementById(form_id);
-    let results = [];
+    let feedback = {};
     let all_valid = true;
 
     for (let i = 0; i < ele.length; i++ ) {
-        const {type, value, required} = ele[i];
+        const {id, type, value, required} = ele[i];
 
         if (required && valid_types.includes(type)) {
-            results[i] = validations[type](value);
-            all_valid = all_valid && results[i].valid;
+            const rev = validations[type](value);
+            feedback[id] = rev.feedback;
+            all_valid = all_valid && rev.valid;
         }
     }
-    return({valid: all_valid, results: results});
+    return({valid: all_valid, feedback: feedback});
 };
--- src/views/auth/login.js
+++ src/views/auth/login.js
@@ -1,15 +1,9 @@
 import React, {useContext, useState} from 'react';
 import {Link} from 'react-router-dom';
 import { Context } from '../../store/appContext';
-import { validate_all } from '../../helper/validations';
+import { validate_all, validate_field, fb_styles } from '../../helper/validations';
 
 export const Login = () => {
-
-    const fb_styles= { // util para mostrar feedback al usuario en los formularios.
-        valid: "invalid-tooltip",
-        invalid: "invalid-tooltip active",
-        neutral: "invalid-tooltip"
-    };
 
     const {store, actions} = useContext(Context); //global store
 
@@ -19,78 +13,60 @@ export const Login = () => {
             login_passw: "", // id del campo password del formulario, contiene el input del usuario.
         },
         rq_fields_fb: { // key de cada objeto debe ser igual al id en constante "required_fields"
-            login_email: {class: fb_styles.neutral, msg: ""}, // contiene el feedback al usuario.
-            login_passw: {class: fb_styles.neutral, msg: ""} // contiene el feedback al usuario
+            login_email: {class: fb_styles.valid, msg: ""}, // contiene el feedback al usuario.
+            login_passw: {class: fb_styles.valid, msg: ""} // contiene el feedback al usuario
         },  
         password_type: "password" // flag para mostrar/ocultar contraseña.
     });
 
-    // const check_valid = (type, id, value) => { 
-    //     // funcion que se encarga de validar a través de type, id (que coincida con los requeridos)
-    //     // y un valor.
-    //     let fb = state.rq_fields_fb;
-    //     required_fields.forEach((item) => { 
-    //         if (id === item.id) { // revisa si el campo necesita validación
-    //             const check = validations[type](value); // se realiza la validación.
-    //             if (check !== undefined) {
-    //                 fb = Object.assign(state.rq_fields_fb, {
-    //                     [id]: {class: fb_styles[check.class], msg: check.msg}
-    //                 });
-    //             };
-    //         };
-    //     });
-    //     return fb;
-    // };
-
     const handleSubmit = (event) => { 
         // se realiza validación de todos los requeridos y si todos son validos, se procede con el submit
         event.preventDefault();
-        console.log(validate_all(event.target.id));
 
-        // let new_fb = state.rq_fields_fb;
+        const valid_form = validate_all(event.target.id);
+        const new_fb = Object.assign(state.rq_fields_fb, valid_form.feedback);
 
-        // const fb_val = required_fields.map((item) => {
-        //     const check = item.validation(state.fields[item.id]);
-        //     new_fb = Object.assign(state.rq_fields_fb, {
-        //         [item.id]: {class: fb_styles[check.class], msg: check.msg}
-        //     });
-        //     return check.valid;
-        // });
-        // setState({
-        //     rq_fields_fb: new_fb,
-        //     ...state
-        // });
-        // if (fb_val.includes(false)) { // si uno de los campos no es válido no permite el envío de form a API
-        //     console.log("no cumple");
-        // } else {
-        //     const result = actions.loadSomeData(); // envío de formulario a API - Recibe mensajes desde backend y muestra feedback en formulario en caso de algún error.
-        //     console.log(result);
-        // };
+        setState({
+            rq_fields_fb: new_fb,
+            ...state
+        });
+
+        if (valid_form.valid) { // si todos los campos requeridos fueron validados
+            const result = actions.loadSomeData(); // envío de formulario a API - Recibe mensajes desde backend y muestra feedback en formulario en caso de algún error.
+            console.log(result);
+        } else {
+            console.log("no cumple");
+        };
     };
 
     const handleChange = (event) => { 
         // función que guarda y valida los inputs del usuario, además realiza validación
         //con cada cambio del input.
+        const {id, value} = event.target;
+
         const new_field = Object.assign(state.fields, {
-            [event.target.id]: event.target.value
+            [id]: value
         });
-        // const new_fb = check_valid(event.target.type, event.target.id, event.target.value); // se llama a la función de validacion según el tipo del campo que genero el evento.
-        
+
         setState({
             fields: new_field,
-            // rq_fields_fb: new_fb,
             ...state
         });
+
+        check_field(event);
     };
 
-    const valid_on_blur = (event) => {
+    const check_field = (event) => {
         //Función que valida al salir del foco de un input (OnFocusOut)
-        // const new_fb = check_valid(event.target.type, event.target.id, event.target.value); // revisa validación del campo según el tipo
+        const {id, type, value} = event.target;
 
-        // setState({
-        //     rq_fields_fb: new_fb,
-        //     ...state
-        // });
+        const new_fb = Object.assign(state.rq_fields_fb, {
+            [id]: validate_field(type, value).feedback
+        })
+        setState({
+            rq_fields_fb: new_fb,
+            ...state
+        });
     }
 
     // const show_password = () => {
@@ -134,7 +110,7 @@ export const Login = () => {
                             id="login_email"
                             value={state.fields.login_email || ""}
                             onChange={handleChange}
-                            onBlur={valid_on_blur}
+                            onBlur={check_field}
                             disabled={store.loading_API}
                             autoComplete="true"
                             required={true}
@@ -150,7 +126,7 @@ export const Login = () => {
                             id="login_passw"
                             value={state.fields.login_passw || ""}
                             onChange={handleChange}
-                            onBlur={valid_on_blur}
+                            onBlur={check_field}
                             disabled={store.loading_API}
                             required={true}
                         />
